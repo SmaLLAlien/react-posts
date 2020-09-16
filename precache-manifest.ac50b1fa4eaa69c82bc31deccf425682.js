3052a053ec5a8e1aaeaea93cba1f01b3fc276488
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2020-09-16 21:58:16 +0300

    Updates

---
+++ precache-manifest.ac50b1fa4eaa69c82bc31deccf425682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cfd595e96f996a112fed5e738e4ef7",
+    "url": "/react-posts/index.html"
+  },
+  {
+    "revision": "e2ea0a757f033f062835",
+    "url": "/react-posts/static/js/2.f8ffc27d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-posts/static/js/2.f8ffc27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab917dfe6b412de059f",
+    "url": "/react-posts/static/js/main.08b64bae.chunk.js"
+  },
+  {
+    "revision": "8c7055c1fdca6d6053b9",
+    "url": "/react-posts/static/js/runtime-main.75bcd501.js"
+  }
+]);
